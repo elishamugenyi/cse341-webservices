eac541a80bac2ef14593205edc8f3360d3daf99c
elishamugenyi <elishamugenyi5@gmail.com>
2024-09-22 00:11:00 +0300

    add

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const app = express();
- 
-app.get('/', (req, res) => {
-  res.send("Hello, welcome to the first API");
-});
+const contactsRoute = require('./routes/contacts');
+
+//middleware to parse JSON
+app.use(express.json());
+
+//use the contacts routes
+app.use('/contacts', contactsRoute);
+
+//set up the server
+//app.get('/', (req, res) => {
+//  res.send("Hello, welcome to the first API");
+//});
  
 app.listen(process.env.PORT || 3000, () => {
   console.log('Web Server is listening at port ' + (process.env.PORT || 3000));

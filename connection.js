af4e98c72c963a0701c90f556a200fe5f6f08459
elishamugenyi <elishamugenyi5@gmail.com>
2024-09-21 21:07:00 +0300

    add

---
+++ connection.js
@@ -0,0 +1,37 @@
+const dotenv = require("dotenv")
+
+const {MongoClient} = require('mongodb'); //import mongodb client
+async function main(){
+    /**
+     * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+     * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+     */
+    const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.vgbfw.mongodb.net/`;
+ 
+    //create an instance for the mongodb client noe that we have the uri
+    const client = new MongoClient(uri);
+ 
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+ 
+        // Make the appropriate DB calls
+        await  listDatabases(client);
+ 
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+//implement the listDatabases function
+async function listDatabases(client){
+    databasesList = await client.db().admin().listDatabases();
+ 
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+
+dotenv.config()
+
+main().catch(console.error);
